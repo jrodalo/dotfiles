5f0fa58c6796c0cdaf807742595b9971e87fb429
José Rodríguez <jose@leanmind.es>
2023-07-29 15:05:12 +0100

    Fix Homebrew install

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ echo "Setting up your Mac..."
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 # Update Homebrew recipes
